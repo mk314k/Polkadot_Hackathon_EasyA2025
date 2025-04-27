357cf60933b6d66e23a118813d6a4b2268274b23
robshow <robson.carmo@parkmobile.io>
2025-04-27 03:08:23 -0400

    added home

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ROUTE_CONFIG } from './Routes.tsx';
-import App from './App.tsx';
+
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { EthereumProvider } from './contexts/EthereumContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <EthereumProvider>
-      <App />
       <RouterProvider router={createBrowserRouter(ROUTE_CONFIG)} />
     </EthereumProvider>
   </StrictMode>,
